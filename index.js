aa68909841f3b34d198954878405717b62b9e20a
Trace <tracedelange@me.com>
2021-06-01 10:25:33 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(value1, value2) {
+    return value1 + value2
+}
+
+function subtract(value1, value2) {
+    return value1 - value2
+}
+
+function multiply(value1, value2) {
+    return value1 * value2
+}
+
+function divide(value1, value2) {
+    return value1 / value2
+}
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
